f36ffa8fcba7828dd6ea5eebb139ea3fc32bc212
mtaku3 <m.taku3.1222@gmail.com>
2023-09-25 09:41:06 +0000

    feat: implement i18n middleware

---
+++ locales.ts
@@ -0,0 +1,2 @@
+export const locales = ["ja", "en"];
+export const defaultLocale = locales[0];
---
+++ middleware.ts
@@ -0,0 +1,55 @@
+import { defaultLocale, locales } from "@/locales";
+import { match } from "@formatjs/intl-localematcher";
+import Negotiator from "negotiator";
+import type { NextRequest } from "next/server";
+
+// Get the preferred locale
+function getLocale(acceptLanguage: string): string {
+  const headers = {
+    "accept-language": acceptLanguage,
+  };
+  const languages = new Negotiator({ headers }).languages();
+  return match(languages, locales, defaultLocale);
+}
+
+export function middleware(request: NextRequest) {
+  // Check if the incoming request is an image
+  const { pathname } = request.nextUrl;
+  if (
+    pathname.endsWith(".png") ||
+    pathname.endsWith(".jpg") ||
+    pathname.endsWith(".jpeg") ||
+    pathname.endsWith(".svg") ||
+    pathname.endsWith(".gif") ||
+    pathname.endsWith(".webp")
+  )
+    return;
+
+  // Check if there is any supported locale in the pathname
+  const pathnameHasLocale = locales.some(
+    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`,
+  );
+
+  if (pathnameHasLocale) return;
+
+  // Redirect if there is no locale
+  const acceptLanguage = request.headers.get("accept-language") ?? "";
+  const locale = getLocale(acceptLanguage);
+  request.nextUrl.pathname = `/${locale}${pathname}`;
+  // e.g. incoming request is /products
+  // The new URL is now /en-US/products
+  return Response.redirect(request.nextUrl);
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|admin).*)",
+  ],
+};
