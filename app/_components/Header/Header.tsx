4f91cfbcc37f886aabcaf72be1bda3d601107801
mtaku3 <m.taku3.1222@gmail.com>
2023-10-08 09:31:27 +0000

    style: remove icon from header

--- app/_components/Header/Header.tsx
+++ app/_components/Header/Header.tsx
@@ -5,13 +5,6 @@ import Image from "next/image";
 export default function Header() {
   return (
     <header className="col-span-full my-4 flex items-center space-x-2">
-      <Image
-        className="h-10 w-10 rounded-full md:h-16 md:w-16"
-        src="/mtaku3.webp"
-        width={40}
-        height={40}
-        alt="Image of mtaku3"
-      />
       <div className="!ml-auto space-x-2">
         <button className="rounded-md p-1 transition hover:bg-slate-100 dark:hover:bg-slate-600">
           <Search className="h-5 w-5 md:h-6 md:w-6" />
