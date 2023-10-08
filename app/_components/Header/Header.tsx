494a749609ac15ede463aba9d0db2e00e61a78f0
mtaku3 <m.taku3.1222@gmail.com>
2023-10-08 09:32:33 +0000

    feat: remove search and rss icon from header due to not implemented

--- app/_components/Header/Header.tsx
+++ app/_components/Header/Header.tsx
@@ -3,16 +3,18 @@ import { Rss, Search } from "lucide-react";
 import Image from "next/image";
 
 export default function Header() {
+  // TODO: Implement search
+  // TODO: Implement RSS feed
   return (
     <header className="col-span-full my-4 flex items-center space-x-2">
       <div className="!ml-auto space-x-2">
-        <button className="rounded-md p-1 transition hover:bg-slate-100 dark:hover:bg-slate-600">
+        {/* <button className="rounded-md p-1 transition hover:bg-slate-100 dark:hover:bg-slate-600">
           <Search className="h-5 w-5 md:h-6 md:w-6" />
-        </button>
+        </button> */}
         <ThemeSwitch className="rounded-md p-1 transition hover:bg-slate-100 dark:hover:bg-slate-600" />
-        <button className="rounded-md p-1 transition hover:bg-slate-100 dark:hover:bg-slate-600">
+        {/* <button className="rounded-md p-1 transition hover:bg-slate-100 dark:hover:bg-slate-600">
           <Rss className="h-5 w-5 md:h-6 md:w-6" />
-        </button>
+        </button> */}
       </div>
     </header>
   );
