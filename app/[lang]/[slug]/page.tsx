4ee5308f4dd676c6b1007eb65fb6f7e26a9173b0
mtaku3 <m.taku3.1222@gmail.com>
2023-10-09 14:37:36 +0000

    fix: provisional fix of invalid return type

--- app/[lang]/[slug]/page.tsx
+++ app/[lang]/[slug]/page.tsx
@@ -7,7 +7,7 @@ import { draftMode } from "next/headers";
 type Props = {
   params: {
     lang: string;
-    slug: string;
+    slug?: string;
   };
 };
 
@@ -34,19 +34,20 @@ export default async function Page({ params }: Props) {
 
 export async function generateStaticParams() {
   const pagesResponse = await client.queries.pageConnection();
-  const pages = pagesResponse.data.pageConnection.edges?.map((page) => {
-    const lang = page?.node?._sys.relativePath?.split("/")[0];
+  const pages =
+    pagesResponse.data.pageConnection.edges?.map((page) => {
+      const lang = page?.node?._sys.relativePath?.split("/")[0];
 
-    let slug = page?.node?._sys.filename;
-    if (slug?.startsWith("home")) {
-      slug = undefined;
-    }
+      let slug = page?.node?._sys.filename;
+      if (slug?.startsWith("home")) {
+        slug = undefined;
+      }
 
-    return {
-      lang,
-      slug,
-    };
-  });
+      return {
+        lang,
+        slug,
+      };
+    }) ?? [];
   return pages;
 }
 
