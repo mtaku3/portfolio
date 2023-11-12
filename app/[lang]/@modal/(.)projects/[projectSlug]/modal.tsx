28a50603e1a929a4ab1dcbe27d212f72bb46b8d9
mtaku3 <m.taku3.1222@gmail.com>
2023-11-12 04:21:42 +0000

    feat: add maximize button for preview modal

--- app/[lang]/@modal/(.)projects/[projectSlug]/modal.tsx
+++ app/[lang]/@modal/(.)projects/[projectSlug]/modal.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import Modal from "@/app/_components/Modal";
-import { X } from "lucide-react";
+import clsx from "clsx";
+import { Maximize2, Minimize2, X } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 type Props = {
   children: React.ReactNode;
@@ -12,13 +14,36 @@ export default function PreviewModal({ children }: Props) {
   const router = useRouter();
   const close = () => router.back();
 
+  const [isMinimized, setIsMinimized] = useState<boolean>(true);
+
+  const minimizedStyle =
+    "md:h-auto md:max-h-[90%] md:w-[640px] md:rounded-md md:border md:border-gray-300 md:dark:border-gray-800 lg:w-[768px] xl:w-[1024px]";
+
   return (
     <Modal isOpen={true} close={close}>
-      <div className="h-full overflow-y-scroll bg-noise-white p-8 dark:bg-noise-dark md:h-auto md:max-h-[90%] md:w-[640px] md:rounded-md md:border md:border-gray-300 md:dark:border-gray-800 lg:w-[768px] xl:w-[1024px]">
-        <div className="mb-8 space-y-4 md:hidden">
-          <button onClick={close}>
-            <X className="h-6 w-6" />
-          </button>
+      <div
+        className={clsx(
+          "h-full overflow-y-scroll bg-noise-white p-8 dark:bg-noise-dark",
+          isMinimized && minimizedStyle,
+        )}
+      >
+        <div className="mb-8 space-y-4">
+          <div className="flex">
+            <button onClick={close}>
+              <X className="h-6" />
+            </button>
+            <div className="ml-auto hidden md:inline-block">
+              {isMinimized ? (
+                <button onClick={() => setIsMinimized(false)}>
+                  <Maximize2 className="h-6" />
+                </button>
+              ) : (
+                <button onClick={() => setIsMinimized(true)}>
+                  <Minimize2 className="h-6" />
+                </button>
+              )}
+            </div>
+          </div>
           <hr className="border-t border-gray-300" />
         </div>
         {children}
