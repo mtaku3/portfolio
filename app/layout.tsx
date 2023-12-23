d5c463f29e45a8224120e20b70f186b6ef9e5b37
mtaku3 <m.taku3.1222@gmail.com>
2023-12-23 21:05:53 +0900

    feat: implement vercel web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { locales } from "@/locales";
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -24,6 +25,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body>{children}</body>
+      <Analytics />
     </html>
   );
 }
